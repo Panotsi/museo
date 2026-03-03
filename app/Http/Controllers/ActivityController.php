22d4e4fb5db9c2d2cfb1d5e949d042237f9a39c3
Panotsi <ocfemiaarjay30@gmail.com>
2026-03-03 08:44:44 +0800

    UPDATE

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Activity;
 
-class ActivityController extends Controller
+class ActivityController
 {
     public function show($id)
     {
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Activity;
 
-class HomeController extends Controller
+class HomeController
 {
     public function index()
     {
