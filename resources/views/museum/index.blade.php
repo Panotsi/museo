78a408c7b34e464ac79530418f7132f6f3c38665
Panotsi <ocfemiaarjay30@gmail.com>
2026-03-16 11:20:52 +0800

    updit

--- resources/views/museum/index.blade.php
+++ resources/views/museum/index.blade.php
@@ -29,7 +29,7 @@
                     <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="#activities">Activities</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Exhibits</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Collections</a></li>
                     <li class="nav-item"><a class="nav-link" href="#visit">Visit</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
                 </ul>
