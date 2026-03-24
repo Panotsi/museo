7d466f181977a6193dd039cbc050fc8aedbbc536
Panotsi <ocfemiaarjay30@gmail.com>
2026-03-24 08:20:41 +0800

    favicon icon

--- resources/views/museum/index.blade.php
+++ resources/views/museum/index.blade.php
@@ -12,7 +12,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Playfair+Display:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-    
+    <!-- Favicon -->
+     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
